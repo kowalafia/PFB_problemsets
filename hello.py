39d03506b6ccb8a544b470ffcf5e28238ef5be98
pfb02 <pfb02@info02.cshl.edu>
2023-10-17 13:27:29 -0400

    problem set 1

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
+
+import sys
+
 print ('My name: Thompson')
+
+
+print ('My favourite color: Green')
+print ('My favorite activity: Coding')
+print ('My favorite animal: Chicken')
+name = 'Pat'
+print(f"My name: {name}") 
+myname = sys.argv[1]
+myfavoritecolor = sys.argv[2]
+myfavoriteactivity = sys.argv[3]
+myfavoriteanimal = sys.argv[4]
+print (myname,  myname, myfavoritecolor, sys.argv[0], myname, myfavoriteactivity, myfavoriteanimal)
+print (myname+  myname+ myfavoritecolor+ sys.argv[0]+ myname+ myfavoriteactivity+ myfavoriteanimal)
+
 
