7e29f326036afdbee736ad2c0777a8f459b4d288
pfb02 <pfb02@info02.cshl.edu>
2023-10-17 15:58:13 -0400

    problem22set

---
+++ problem22.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+count = 60
+if count < 0:
+        message = "Negative"
+        print(message)
+elif count < 50:
+        message = "Negative"
+        print(message)
+elif count > 50:
+        message = "Positive"
+        print(message)
+else:
+        message = "Negative"
+        print(message)
+
