03204b0228c30b0a358b6527425053063efacc0b
pfb02 <pfb02@info02.cshl.edu>
2023-10-17 15:39:00 -0400

    problem2set

---
+++ problem2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+count = 60
+if count < 0:
+        message = "Thumbs down"
+        print(count, message)
+elif count < 50:
+        message = "Not true"
+        print(count, message)
+elif count > 50:
+        message = "Thumbs up"
+        print(count, message)
+else:
+        message = "must be 50"
+        print(count, message)
+
+
